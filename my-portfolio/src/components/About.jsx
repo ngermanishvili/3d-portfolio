45827895974dbe61f9674c2ab34e812100ff9220
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-03-14 20:19:57 +0400

    done about section
    done about

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -4,9 +4,31 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
-const ServiceCard = ({ title, Icon, index }) => {
-  return <p> {title} </p>;
+const ServiceCard = ({ title, icon, index }) => {
+  return (
+    <Tilt className="xs:w-[250px] f-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 25,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt="title" className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center ">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
 };
 
 const About = () => {
@@ -29,13 +51,13 @@ const About = () => {
         creating visually stunning websites and applications. Age 22.
       </motion.p>
 
-      <div className="mt-2 flex flex-wrap gap-10">
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...services} />
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
     </>
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
