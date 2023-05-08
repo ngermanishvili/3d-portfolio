409f581ea56d4af97aec421ef959435c45f25891
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-08 23:59:10 +0400

    update hero styles

--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
-        </div>
+        </div>    
 
         <div className={`${styles.heroHeadText}`}>
           <h1>
