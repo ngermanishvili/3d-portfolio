d604188430600b0b18912c40800ddbcc04c7627d
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-08 23:58:47 +0400

    refactor navbar

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Nika &nbsp;
+            Nika &nbsp; 
             <span className="sm:block hidden"> | Germanishvili &nbsp; </span>
           </p>
         </Link>
